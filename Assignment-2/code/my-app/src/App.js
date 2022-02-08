aa66182515420fc1240d51f92801583683cc5d75
akhilc120 <akhilreddymannem8@gmail.com>
2022-02-08 19:34:22 +0530

    my react2

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
-
-
 function App(props) {
- const {employee}=props;
-    
-return(
-  <div className="container">
-  {/* Start your React code here */}
-</div>
-)
+  const { employee, title } = props;
+  return (
+    <div className="container App" >
+      <div className="profile-image-container">
+        <img className="profile" src={employee.profileImg} width="100" height="100" alt="profile image sample">
+        </img>
+      </div>
+      <div className="title">
+        <h1>{employee.name}</h1>
+      </div>
+      <div className="profile-info">
+        <label>Location</label>
+        <strong>{employee.location}</strong>
+      </div>
+      <div className="profile-info">
+        <label>BloodGroup</label>
+        <strong>{employee.bloodGroup}</strong>
+      </div>
+      <div className="profile-info">
+        <label>Age</label>
+        <strong>{employee.age}</strong>
+      </div>
+</div >
+);
 }
-
-
-
 export default App;
